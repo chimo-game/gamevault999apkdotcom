5b448be13e04d254bd58cafd12b2614acdbf30a7
tolapao <62795957+XANuN@users.noreply.github.com>
2023-04-12 20:13:04 +0700

    a

--- assets/js/random_color.js
+++ assets/js/random_color.js
@@ -1,7 +1,8 @@
 function changeColor() {
     var colors = [
-        "#C6DDF0", "#F06449", "#5BC3EB", "#540D6E", "#EE4266", "#0EAD69",
-        "#E0FF4F", "#FF2ECC", "#00272B", "#00A6FB", "#F02D3A", "#D9BBF9"
+        "#DB2B39", "#F06449", "#5BC3EB", "#540D6E", "#EE4266", "#7E2E84",
+        "#1B4079", "#FF2ECC", "#38369A", "#00A6FB", "#F02D3A", "#29335C",
+        "#C42021", "#3C1742", "#020887", "#003E1F", "#390099", "#BA1F33"
     ];
     var color = colors[Math.floor(Math.random() * colors.length)];
     document.querySelector("#homepage_hero_party").style.color = color;
